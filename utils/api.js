57decb84f2f6947750a9faa4316939e84a98f006
Greg Stead <gregstead@gmail.com>
2021-02-02 17:37:02 -0600

    finishes API

--- utils/api.js
+++ utils/api.js
@@ -2,15 +2,7 @@ import axios from "axios";
 
 // Export and object containing API methods
 export default {
-  getRandomEmployee: function () {
-    return axios.get("https://randomuser.me/api/");
-  },
-  getRandomEmployees: async function () {
-    let employees = [];
-    for (let i = 0; i < 10; i++) {
-      const employee = await this.getRandomEmployee();
-      employees.push(employee);
-    }
-    return employees;
+  getRandomEmployees: function () {
+    return axios.get("https://randomuser.me/api/?results=20");
   },
 };
